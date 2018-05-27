5755f2c9ec60a9f8d03b3d8910fa6f737cccc924
Leo <tafpro@gmail.com>
2018-05-27 14:31:06 -0600

    Added methods.

--- src/main/java/tester/SimpleMath.java
+++ src/main/java/tester/SimpleMath.java
@@ -1,4 +1,20 @@
 package tester;
 
-public class SimpleMath {
+public abstract class SimpleMath {
+
+    public Integer Add(Integer first, Integer second) {
+        return first + second;
+    }
+
+    public Integer Subtract(Integer first, Integer second) {
+        return first - second;
+    }
+
+    public Integer Multiply(Integer first, Integer second) {
+        return first * second;
+    }
+
+    public Integer Divide(Integer first, Integer second) {
+        return first / second;
+    }
 }
